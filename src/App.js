df027516800243b597cd88cafbeb1003aaa2b318
Kevin Lamberton <kevinlamberton@MacBook-Pro-de-Kevin-Lamberton.local>
2019-04-09 21:17:36 +0200

    premiers tests réussis pour le message de congrats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Congrats from "./Congrats";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Congrats />
       </div>
     );
   }
